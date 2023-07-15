140fe5a099c9c72a1baa1d85ad334c7b72e034c8
Guilherme Henrique de Miranda <miranda.guilherme@made4it.com.br>
2023-07-15 17:48:39 -0300

    Adiciona criptografia na senha do usuário.

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, Usuario } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
+import { createCipheriv, randomBytes, scrypt } from 'node:crypto';
+import { promisify } from 'node:util';
 
 @Injectable()
 export class UsuariosService {
@@ -19,8 +21,26 @@ export class UsuariosService {
   }
 
   async criarUsuario(data: Prisma.UsuarioCreateInput): Promise<Usuario> {
+    // Gera a senha criptografada
+    const iv = randomBytes(16);
+    const chave = '36201923898983245156487166564765627420101234';
+    const key = (await promisify(scrypt)(chave, 'salt', 32)) as Buffer;
+    const cipher = createCipheriv('aes-256-ctr', key, iv);
+    const senhaDigitada = data.senha;
+    const encryptedText = Buffer.concat([
+      cipher.update(senhaDigitada),
+      cipher.final(),
+    ]);
+    // Converte o Buffer em uma string hexadecimal
+    const senhaCriptografada = encryptedText.toString('hex');
+    // Atualize o tipo do objeto `data` para incluir a propriedade `senha`
+    const dadosUsuario: Prisma.UsuarioCreateInput = {
+      ...data,
+      senha: senhaCriptografada,
+    };
+
     return this.prisma.usuario.create({
-      data,
+      data: dadosUsuario,
     });
   }
 
